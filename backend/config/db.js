bd72ba45e7f5a15cb3aa607294ee3559b65c2edc
FranklynCodes <47542708+FranklynCodes@users.noreply.github.com>
2022-06-23 02:03:42 -0400

    Added log information for db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-console.log(mongoose.version)
+console.log("Mongoose Version " + mongoose.version);
 
 const connectDB = async () => {
-	console.log(process.env.MONGO_URI);
+	console.log("Your mongodb connection key is\n\t " + process.env.MONGO_URI + "\n");
 	try {
 		await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 		});
 		console.log("MongoDB Connection SUCCESS");
 	} catch (error) {
-		console.log("Mongo connection FAIL", error);
+		console.log("Mongo Connection FAIL", error);
 		process.exit(1); // EXITS SERVER
 	}
 };
